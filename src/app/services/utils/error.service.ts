475ed4fd8fa179a499f8732417d6a4005fab4cd2
Gustavo Neris <nerisgs20@gmail.com>
2023-07-08 04:19:41 -0300

    feat: error service

---
+++ src/app/services/utils/error.service.ts
@@ -0,0 +1,21 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { throwError } from 'rxjs';
+
+export class ErrorService {
+
+  constructor() { }
+
+  public static handleError(error: HttpErrorResponse) {
+    let errorMessage = '';
+    console.error(error);
+    if (error.error instanceof ErrorEvent) {
+      errorMessage = error.error.message;
+    } else {
+      errorMessage = `ERRO : ${error.status}, ` + `DS : ${error.message}` + `# : ${error}`;
+    }
+    return throwError(errorMessage);
+
+  };
+
+
+}
