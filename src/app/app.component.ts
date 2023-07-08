5e7acf27cd924bf43890f7db5b262545e890887a
Gustavo Neris <nerisgs20@gmail.com>
2023-07-08 04:18:57 -0300

    innitial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, isDevMode } from '@angular/core';
+import { Router } from '@angular/router';
+import { setTheme } from 'ngx-bootstrap/utils';
+
+interface SideNavToggle {
+  screenWidth: number;
+  collapsed: boolean;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-dashboard';
+
+export class AppComponent implements OnInit {
+
+  title = 'sidenav';
+  isSideNavCollapsed = false;
+  screenWidth = 0;
+
+  constructor(
+    private router: Router
+  ) {
+    setTheme('bs5');
+
+  }
+
+  ngOnInit() {
+
+    if (isDevMode()) {
+      console.info(`%cDevelopment!`, "color: green; font-size: 2em");
+    } else {
+      console.info(`%cHold on!`, "color: red; font-size: 3em");
+      console.info(`%cThis browser feature is intended for developers.\nIf someone instructed you to copy and paste something here to enable some feature or to "hack" someone's account, it usually means he's trying to get access to your account.`, "font-size: 1.5em;");
+      console.info(`%cPlease proceed with caution.`, "color: orange; font-size: 1.5em;");
+    }
+  }
+
+  onToggleSideNav(data: SideNavToggle): void {
+    this.screenWidth = data.screenWidth;
+    this.isSideNavCollapsed = data.collapsed;
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,50 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxMaskDirective, NgxMaskPipe, provideEnvironmentNgxMask } from 'ngx-mask';
+import { ToastrModule } from 'ngx-toastr';
+import { SelectDropDownModule } from 'ngx-select-dropdown';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { ErrorService } from './services/utils/error.service';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { SidenavComponent } from './shared/sidenav/sidenav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    SidenavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    BsDropdownModule.forRoot(),
+    ToastrModule.forRoot({
+      timeOut: 6000,
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: true,
+      progressBar: true
+    }),
+    NgxSpinnerModule.forRoot(),
+    ReactiveFormsModule,
+    NgxMaskDirective,
+    NgxMaskPipe,
+    SelectDropDownModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    ErrorService,
+    provideEnvironmentNgxMask()
+  ],
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule { }
