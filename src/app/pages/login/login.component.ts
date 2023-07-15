72fd5830c08ff3f5b2b14a649aa50a072fdfc4ce
Gustavo Neris <nerisgs20@gmail.com>
2023-07-15 06:19:18 -0300

    feat: add screen login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit, TemplateRef } from '@angular/core';
-import { Form, FormBuilder, Validators } from '@angular/forms';
+import { AbstractControl, Form, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { BsModalRef, BsModalService, ModalOptions } from 'ngx-bootstrap/modal';
 import { ToastrService } from 'ngx-toastr';
+import { User } from 'src/app/models/User';
 import { Results } from 'src/app/models/results';
 import { LoginService } from 'src/app/services/login/login.service';
+import { UserService } from 'src/app/services/user/user.service';
 import { FunctionsService } from 'src/app/shared/functions/functions.service';
+import { ValidationsService } from 'src/app/shared/validations/validations.service';
 
 @Component({
   selector: 'app-login',
@@ -14,16 +17,21 @@ import { FunctionsService } from 'src/app/shared/functions/functions.service';
 export class LoginComponent implements OnInit {
 
   modalRef?: BsModalRef;
-  form: any;
-  form2: any;
+  form!: FormGroup;
+  form2!: FormGroup;
 
   get f(): any {
     return this.form.controls;
   }
 
+  get f2(): any {
+    return this.form2.controls;
+  }
+
   constructor(
     private fb: FormBuilder,
     private loginService: LoginService,
+    private userService: UserService,
     private functionsService: FunctionsService,
     private toastr: ToastrService,
     private bsModalService: BsModalService
@@ -38,27 +46,44 @@ export class LoginComponent implements OnInit {
     localStorage.clear();
 
     this.form = this.fb.group({
-      Email: ['', Validators.required],
-      Password: ['', Validators.compose([Validators.required,Validators.minLength(5),Validators.maxLength(100)])],
+      Email: ['', Validators.compose([
+          Validators.required,
+          ValidationsService.ValidaEmail
+        ])
+      ],
+      Password: ['', Validators.compose([
+          Validators.required,
+          Validators.minLength(5),
+          Validators.maxLength(100)
+        ]
+      )],
       Submited: [false]
     });
 
-    /*this.form2 = this.fb.group({
-      Email: ['', Validators.required],
-      Password: ['', Validators.compose([Validators.required,Validators.minLength(5),Validators.maxLength(100)])],
-      Submited: [false]
-    });
-    */
   }
 
+  matchPass(Password: AbstractControl, PasswordConfirm: AbstractControl) {
+    if(Password.value){
+      let pass = Password.value
+      let confirmPass = PasswordConfirm.value
 
-  login() {
+      if (confirmPass !== pass){
+        PasswordConfirm.setErrors({ matchPass: false })
+      }else{
+        PasswordConfirm.setErrors(null);
+      }
+    }
+    return null;
+
+  }
+
+  Sign() {
     if (this.form.invalid){
-      this.form.controls.Submited.setValue(true);
+      this.form.controls['Submited'].setValue(true);
       this.toastr.error('Existe um ou mais campos pendentes ou com preenchimento incorreto.');;
       return
     }else{
-      this.form.controls.Submited.setValue(false);
+      this.form.controls['Submited'].setValue(false);
 
       const args = {
         ...this.form.getRawValue()
@@ -78,14 +103,58 @@ export class LoginComponent implements OnInit {
         },
       }
     )}
-
   }
+
+
+    SignUp() {
+      if (this.form2.invalid){
+        this.form2.controls['Submited'].setValue(true);
+        this.toastr.error('Existe um ou mais campos pendentes ou com preenchimento incorreto.');;
+        return
+      }else{
+        this.modalRef?.hide()
+
+        this.form2.controls['Submited'].setValue(false);
+
+        const args = {
+          ...this.form2.getRawValue()
+        }
+
+        this.userService.CreateUser(args).subscribe({
+          next: (_dados: Results<User>) => {
+            if (_dados.success) {
+              this.toastr.success('Usuario Cadastrado!').onHidden.subscribe((...args) => {
+                console.log(args);
+              });
+            }else{
+              this.toastr.error(_dados.data);
+            }
+          },
+          error: (error: any) => {
+            this.toastr.error(error);
+          },
+          complete: () => {
+          },
+        })
+      }
+    }
+
+
 
   async openModal(
     evento: any,
     template: TemplateRef<any>,
   ) {
     evento.stopPropagation();
+
+    this.form2 = this.fb.group({
+      FirstName: ['', Validators.required],
+      LastName: ['', Validators.required],
+      Email: ['', Validators.compose([Validators.required, ValidationsService.ValidaEmail])],
+      Password: ['', Validators.required],
+      PasswordConfirm: ['', Validators.required],
+      Submited: [false]
+    });
 
     const modalConfig: ModalOptions = { class: 'modal-lg', backdrop: 'static' }
 
