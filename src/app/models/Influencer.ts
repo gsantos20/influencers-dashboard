6e8efb424aea297b5eb2318f816fc622112dd753
Gustavo Neris <nerisgs20@gmail.com>
2023-07-15 19:15:07 -0300

    feat: add models Influencer and Plataform

---
+++ src/app/models/Influencer.ts
@@ -0,0 +1,9 @@
+export interface Influencer {
+  _id: string
+  NameInfluencer: string
+  EmailInfluencer: string
+  ChannelUsername: string
+  PlatformId: number
+  NumSubs: number
+  ContentCategory: string
+}
---
+++ src/app/models/Platform.ts
@@ -0,0 +1,5 @@
+export interface Platform {
+  PlatformId: number
+  PlatformDs: string
+  Active: string
+}
